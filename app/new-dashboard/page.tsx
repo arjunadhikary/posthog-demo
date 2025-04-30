d4984ba0ecb1b1d0ef75d16f6ac7cdd8b82d3328
arjun-rumsan <arjun@rumsan.net>
2025-04-30 17:09:44 +0545

    :remove unused import of useTransition

--- app/new-dashboard/page.tsx
+++ app/new-dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, useTransition } from "react";
+import { useEffect, useState } from "react";
 import posthog from "@/posthog.config";
 import { useDashboardVariantRedirect } from "../hooks/useDashboardVariantRedirect";
 
